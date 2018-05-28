77a75da3ee6a2533a746b908624f0dc0a7c6de1e
DoktorMc <38439374+DoktorMc@users.noreply.github.com>
2018-05-28 10:46:01 +0300

    Add BL Library, UserDTO, AutoMapper, and minor changed

---
+++ BL/Helper/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ClassLibrary;
+using ClassLibrary.DTOS;
+
+namespace BL.Helper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+        }
+    }
+        
+}
